620b258f1b95ef7361b12a0afef04ee63c010639
Vicent Esteve <esteve.ferre.vicent@gmail.com>
2023-02-10 16:12:24 +0100

    Add Changes

--- src/test/api.test.js
+++ src/test/api.test.js
@@ -4,6 +4,6 @@ const CalcCuadrado = require("./CalcCuadrado")
 
 test("Test para calcular area de los poligonos", () => {
   expect(CalcTriangulo(2, 3)).toBe(3);
-  expect(CalcCirculo(4)).toBe(50.26548245743669);
+  expect(CalcCirculo(4)).toBe(50.27);
   expect(CalcCuadrado(2, 3)).toBe(6);
 });
