acd2035d9d1e2df8dcf71e3ef0a668b765f7f617
Vicent Esteve <esteve.ferre.vicent@gmail.com>
2023-02-11 23:23:53 +0100

    updated

--- jenkinsScripts/deploy_vercel.sh
+++ jenkinsScripts/deploy_vercel.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [ "$2" == "0" ] && [ "$3" == "0" ] && [ "$4" == "0" ] && [ "$5" == "0" ] && [ "$6" == "0" ]; then
-  vercel --token "$1" --yes --name jenkins-react-poligons
+    echo Hola soy el if
+#   vercel --token "$1" --yes --name jenkins-react-poligons
 else
   exit 1
 fi
