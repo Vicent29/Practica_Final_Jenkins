e73ac9bc05960cd3893b3061a3d88751fa45be0c
esteve.ferre.vicent@gmail.com <Vicent29>
2023-02-12 14:23:08 +0100

    fix errors

--- jenkinsScripts/send_notification.js
+++ jenkinsScripts/send_notification.js
@@ -1,18 +1,18 @@
 // Variables mailgun crear formulario
-var API_KEY_MAILGUN = process.argv[1]
-var DOMAIN_MAILGUN = process.argv[2]
-var DESTINATARIO = process.argv[3]
+var API_KEY_MAILGUN = process.argv[3]
+var DOMAIN_MAILGUN = process.argv[4]
+var DESTINATARIO = process.argv[5]
 
 const formData = require('form-data');
 const Mailgun = require('mailgun.js');
 const form_mailgun = new Mailgun(formData);
-const send_message = form_mailgun.client({ username: 'api', key: API_KEY_MAILGUN, url:"https://api.eu.mailgun.net"});
+const send_message = form_mailgun.client({ username: 'api', key: API_KEY_MAILGUN});
 
 //Variables de estado de los jobs
-let result_Linter_stage = process.argv[4] == 0 ? "success": "failure";
-let resultado_Test_stage = process.argv[5] == 0 ? "success": "failure";
-let result_Update_readme_stage = process.argv[6] == 0 ? "success": "failure";
-let result_Deploy_to_Vercel_stage = process.argv[7] == 0 ? "success": "failure";
+let result_Linter_stage = process.argv[6] == 0 ? "success": "failure";
+let resultado_Test_stage = process.argv[7] == 0 ? "success": "failure";
+let result_Update_readme_stage = process.argv[8] == 0 ? "success": "failure";
+let result_Deploy_to_Vercel_stage = process.argv[9] == 0 ? "success": "failure";
 
 
 let asunto= "Resultado de la pipeline ejecutada"
